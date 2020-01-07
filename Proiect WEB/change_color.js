9e2d19ff130ade50fd362d536471cc86f4fb2cc4
Pascu Bianca <bianca.pascu1999@yahoo.com>
2020-01-07 22:34:57 +0200

    final js

--- Proiect WEB/change_color.js
+++ Proiect WEB/change_color.js
@@ -1,7 +1,5 @@
 function schimb_light() {
     var element = document.getElementById("id_mod");
-    //console.log(window.location.pathname);
-
     document.getElementById("id_mod").href = "style_light_mode.css";
     document.getElementById('img_sigla').src = 'poze_light_mode/sigla.svg';
     if (typeof(Storage) !== "undefined") {
@@ -15,21 +13,24 @@ function schimb_light() {
 
 function schimb_red() {
     var element = document.getElementById("id_mod");
-    //console.log(window.location.pathname);
-
     document.getElementById("id_mod").href = "style_red.css";
     document.getElementById('img_sigla').src = 'poze_mod_culoare_2/sigla_general.svg';
-    // if (document.getElementById('sigla_biography').src != NULL)
-    //     document.getElementById('sigla_biography').src = 'poze_mod_culoare_2/sigla_biography.svg';
-    // else
-    // if (document.getElementById('sigla_books').src != NULL)
-    //     document.getElementById('sigla_books').src = 'poze_mod_culoare_2/sigla_books.svg';
-
     if (typeof(Storage) !== "undefined") {
 
         localStorage.tema = "style_red.css";
 
 
+    }
+
+}
+
+function schimb_dark() {
+    var element = document.getElementById("id_mod");
+    document.getElementById("id_mod").href = "style.css";
+    document.getElementById('img_sigla').src = 'poze/sigla.svg';
+    if (typeof(Storage) !== "undefined") {
+
+        localStorage.tema = "style.css";
     }
 
 }
@@ -42,34 +43,79 @@ function checkCookies() {
     if (mode === "style_light_mode.css") {
         schimb_light();
     }
-
+    if (mode === "style.css") {
+        schimb_dark();
+    }
 }
-/* When the user clicks on the button, 
-                                                                                               toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//     if (!event.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
-
-// Get the modal
-var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
+    var modal = document.getElementById('id01');
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+
+
+function onClickCancel() {
+    document.getElementById('id01').style.display = 'none';
+    document.getElementById("usernameID").value = "";
+    document.getElementById("passwordID").value = "";
+}
+
+// buton scroll
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+//Get the button
+
+
+function scrollFunction() {
+
+    var mybutton = document.getElementById("myBtn");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//alert mail
+
+function alertMail() {
+    var myVar;
+    myVar = setTimeout(alertFunc, 1000);
+}
+
+function alertFunc() {
+    alert("Ați fost adaugat in lista de abonați.");
+}
+
+//dropdown teme
+
+function Drop_teme() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 }
